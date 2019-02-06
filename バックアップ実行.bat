16279c53d5bacdd2da18ecabb7079dde8115d328
yamagami2211 <yamagami2211@svk.jp>
2019-02-06 15:30:38 +0900

    %USERNAME%に変更

--- バックアップ実行.bat
+++ バックアップ実行.bat
@@ -1,6 +1,6 @@
 @ECHO OFF
 mkdir %date:~-10,4%%date:~-5,2%%date:~-2,2%
 cd %~dp0%date:~-10,4%%date:~-5,2%%date:~-2,2%
-copy "C:\Users\<ユーザー名>\AppData\Local\Google\Chrome\User Data\Default\Bookmarks.bak"
-copy "C:\Users\<ユーザー名>\AppData\Local\Google\Chrome\User Data\Default\Bookmarks"
+copy "C:\Users\%USERNAME%\Appdata\Local\Google\Chrome\User Data\Default\Bookmarks.bak"
+copy "C:\Users\%USERNAME%\Appdata\Local\Google\Chrome\User Data\Default\Bookmarks"
 PAUSE
